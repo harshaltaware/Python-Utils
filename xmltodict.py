4df2049c16ef53373f4288e1b68f261d36712fa4
Harshal Taware <34537492+harshaltaware@users.noreply.github.com>
2021-02-08 13:39:15 +0530

    Update xmltodict.py

--- xmltodict.py
+++ xmltodict.py
@@ -6,3 +6,11 @@ dict2 = xmltodict.parse(s,force_list={'intervention_browse'})
 print json.dumps(dict2)
 with open('sample.json','wb') as fb:
     json.dumps(dict2, fb)
+
+    
+import json
+import xmlschema
+xs = xmlschema.XMLSchema('schema.xml')
+path = '<file>.xml'
+print xs.is_valid(path)
+print json.dumps(xs.to_dict(path))
