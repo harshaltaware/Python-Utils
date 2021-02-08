954587124600e2ec03461d6a2d8d81d501f7e72f
Harshal Taware <34537492+harshaltaware@users.noreply.github.com>
2021-02-08 13:38:10 +0530

    Create xmltodict.py

---
+++ xmltodict.py
@@ -0,0 +1,8 @@
+import xmltodict, json
+fp1 = open("xml file", 'r')
+s = fp1.read()
+# dict1 = xmltodict.parse('<e> <a>text</a> <a>text</a> </e>')
+dict2 = xmltodict.parse(s,force_list={'intervention_browse'})
+print json.dumps(dict2)
+with open('sample.json','wb') as fb:
+    json.dumps(dict2, fb)
